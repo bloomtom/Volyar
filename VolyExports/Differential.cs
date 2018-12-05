4d4d90751156b9121499853ac87b538085f67acc
Tom Bloom <bloomtom@all8bits.com>
2018-12-05 00:13:39 -0500

    Amend previous fix for serialization

--- VolyExports/Differential.cs
+++ VolyExports/Differential.cs
@@ -14,29 +14,25 @@ namespace VolyExports
         /// The newest available key from the point this differential was created.
         /// If you're tracking the newest changes, this is the key you want to pass back for your next sync operation.
         /// </summary>
-        public long CurrentKey { get; private set; }
+        public long CurrentKey { get; set; }
         /// <summary>
         /// The media items deleted in this differential window.
         /// </summary>
-        public IEnumerable<Deletion> Deletions { get; private set; }
+        public IEnumerable<Deletion> Deletions { get; set; }
         /// <summary>
         /// The media items added in this differential window.
         /// </summary>
-        public IEnumerable<MediaItem> Additions { get; private set; }
+        public IEnumerable<MediaItem> Additions { get; set; }
         /// <summary>
         /// The media items modified in this differential window.
         /// </summary>
-        public IEnumerable<MediaItem> Modifications { get; private set; }
+        public IEnumerable<MediaItem> Modifications { get; set; }
 
         /// <summary>
         /// Creates a new full Differential instance.
         /// </summary>
-        public Differential(long currentKey, IEnumerable<Deletion> deletions, IEnumerable<IMediaItem> additons, IEnumerable<IMediaItem> modifications)
+        public Differential()
         {
-            CurrentKey = currentKey;
-            Deletions = deletions;
-            Additions = additons.Select(x => MediaItem.Convert(x));
-            Modifications = modifications.Select(x => MediaItem.Convert(x));
         }
     }
 }
