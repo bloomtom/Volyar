12bb7022754864f86600991d2de2be0b81dfb698
Tom Bloom <bloomtom@all8bits.com>
2019-01-09 03:30:28 -0500

    Better documentation on Library/ILibrary

--- VolyConverter/Scanning/ILibrary.cs
+++ VolyConverter/Scanning/ILibrary.cs
@@ -3,13 +3,34 @@ using DEnc;
 
 namespace VolyConverter.Scanning
 {
+    /// <summary>
+    /// Represents the configuration for a library directory.
+    /// </summary>
     public interface ILibrary
     {
+        /// <summary>
+        /// If not zero, the output framerate is forced to this value on encode.
+        /// </summary>
         int ForceFramerate { get; }
+        /// <summary>
+        /// The library name. Should be unique.
+        /// </summary>
         string Name { get; }
+        /// <summary>
+        /// The path to source media items from.
+        /// </summary>
         string OriginPath { get; }
+        /// <summary>
+        /// A collection of qualities to encode into.
+        /// </summary>
         IEnumerable<Quality> Qualities { get; }
+        /// <summary>
+        /// The temporary path to store intermediate files when encoding.
+        /// </summary>
         string TempPath { get; }
+        /// <summary>
+        /// The file extensions allowed for uptake in a scan. Should start with a dot (.mp4, .mkv, etc.)
+        /// </summary>
         HashSet<string> ValidExtensions { get; }
 
         string ToString();
--- VolyConverter/Scanning/Library.cs
+++ VolyConverter/Scanning/Library.cs
@@ -5,13 +5,34 @@ using System.Text;
 
 namespace VolyConverter.Scanning
 {
+    /// <summary>
+    /// Represents the configuration for a library directory.
+    /// </summary>
     public class Library : ILibrary
     {
+        /// <summary>
+        /// The library name. Should be unique.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The path to source media items from.
+        /// </summary>
         public string OriginPath { get; set; }
+        /// <summary>
+        /// The temporary path to store intermediate files when encoding.
+        /// </summary>
         public string TempPath { get; set; }
+        /// <summary>
+        /// The file extensions allowed for uptake in a scan. Should start with a dot (.mp4, .mkv, etc.)
+        /// </summary>
         public HashSet<string> ValidExtensions { get; set; }
+        /// <summary>
+        /// A collection of qualities to encode into.
+        /// </summary>
         public IEnumerable<Quality> Qualities { get; set; }
+        /// <summary>
+        /// If not zero, the output framerate is forced to this value on encode.
+        /// </summary>
         public int ForceFramerate { get; set; } = 0;
 
         public Library()
