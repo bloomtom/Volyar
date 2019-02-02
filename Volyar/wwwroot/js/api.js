7b7f78d09262ab41fd7702ae727d4806dc135a9e
Tom Bloom <bloomtom@all8bits.com>
2019-02-02 10:14:07 -0500

    Uses relative URL for API

--- Volyar/wwwroot/js/api.js
+++ Volyar/wwwroot/js/api.js
@@ -1,48 +1,48 @@
 ﻿
 function getStatus(done, fail) {
     $.get(
-        "/external/api/conversion/status"
+        "../external/api/conversion/status"
     ).done(done).fail(fail);
 }
 
 function getTestStatus(done, fail) {
     $.get(
-        "/external/api/conversion/teststatus"
+        "../external/api/conversion/teststatus"
     ).done(done).fail(fail);
 }
 
 function getComplete(done, fail) {
     $.get(
-        "/external/api/conversion/complete"
+        "../external/api/conversion/complete"
     ).done(done).fail(fail);
 }
 
 function cancelItem(item, done, fail) {
     $.post(
-        "/external/api/conversion/cancel?name=" + encodeURIComponent(item)
+        "../external/api/conversion/cancel?name=" + encodeURIComponent(item)
     ).done(done).fail(fail);
 }
 
 function cancelAll(done, fail) {
     $.post(
-        "/external/api/conversion/cancel"
+        "../external/api/conversion/cancel"
     ).done(done).fail(fail);
 }
 
 function pauseQueue(done, fail) {
     $.post(
-        "/external/api/conversion/pause"
+        "../external/api/conversion/pause"
     ).done(done).fail(fail);
 }
 
 function resumeQueue(done, fail) {
     $.post(
-        "/external/api/conversion/resume"
+        "../external/api/conversion/resume"
     ).done(done).fail(fail);
 }
 
 function postFullscan(done, fail) {
     $.post(
-        "/internal/api/scan/fullscan"
+        "../internal/api/scan/fullscan"
     ).done(done).fail(fail);
 }
