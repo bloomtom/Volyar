0eb869263450b39edbc00e0e7763726ca6cc14dd
Tom Bloom <bloomtom@all8bits.com>
2019-11-20 02:00:38 -0500

    Retries on S3 hash mismatch (corrupt upload)

--- VolyFiles/TryDo.cs
+++ VolyFiles/TryDo.cs
@@ -22,9 +22,16 @@ namespace VolyFiles
                 catch (Exception ex) when (
                 ex is TemporaryFailureException ||
                 ex is InvalidOperationException ||
-                ex is TimeoutException
+                ex is TimeoutException ||
+                ex is Amazon.Runtime.AmazonClientException
                 )
                 {
+                    // Check to see if this is a spurious failed upload due to data corruption.
+                    if (ex is Amazon.Runtime.AmazonClientException exa && !exa.Message.Contains("hash not equal to calculated hash"))
+                    {
+                        throw;
+                    }
+
                     if (i >= attempts - 1) { throw; }
                     log?.LogWarning($"Temporary failure: {ex.Message}. Will attempt {attempts - i} more times.");
                 }
